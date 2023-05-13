9d7df87f752dc66d98655c4d27e7cf9bd2e74980
0xAckerMan <joelkores1@gmail.com>
2023-05-13 12:57:44 +0300

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-php-if_else_loops_functions/2_print_alphabets.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+
+//2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
+
+
+for ($alpha = ord('a'); $alpha <= ord('z'); $alpha ++){
+    echo chr($alpha);
+}
+echo "\n";
