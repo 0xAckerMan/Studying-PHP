645164d62d2cf7a76a1e85439e7a9fc972c48b45
0xAckerMan <joelkores1@gmail.com>
2023-05-13 11:30:24 +0300

    Hello world!

--- 0x00-introduction/0-php_is_amazing.php
+++ 0x00-introduction/0-php_is_amazing.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        echo "<h1>","This is switch case","</h1>"
+<?php
 
-        if( $_GET["name"] || $_GET["age"]){
-            echo "Welcome".$_GET["name"]."<br/>";
-            
-        }
-    ?>
-</body>
-</html>
+echo 'Hello world';
